50009a9294a4bd90523b6296e369a5231dcd6b46
vkuleshov-sc <kuleshov@scand.com>
2019-11-29 11:43:04 +0300

    refactore(rename vars and split logic to funcs)

--- GitHubToCBuilder.user.js
+++ GitHubToCBuilder.user.js
@@ -13,19 +13,30 @@
 (function () {
   'use strict';
 
-  const addToc = () => {
-    let textArea = document.getElementsByTagName('textarea')[0];
-    let value = textArea.value;
-    let selectionStart = textArea.selectionStart;
-    let result = '';
+  const getAnchor = text => '#' + text.replace(/ /g, '-').replace(/\/|\.|\(|\)/g, '').toLowerCase();
 
-    value.match(/#+ [^\n]*/g).forEach(h => {
-      let sharpCount = h.match(/#+/)[0].length;
-      let header = h.replace(/#+ /, '');
-      let link = '#' + header.replace(/ /g, '-').replace(/\/|\.|\(|\)/g, '').toLowerCase();
-      result += `${' '.repeat((sharpCount - 1) * 2)}- [${header}](${link})\n`;
+  const getSharpCount = value => value.match(/#+/)[0].length;
+
+  const getHeaderText = headerLine => headerLine.replace(/#+\s+/, '');
+
+  const getHeadersLines = value => value.match(/#+\s+[^\r\n]*/g);
+
+  const createToc = value => {
+    let result = '';
+    getHeadersLines(value).forEach(line => {
+      const sharpCount = getSharpCount(line);
+      const text = getHeaderText(line);
+      const anchor = getAnchor(text);
+      result += `${' '.repeat((sharpCount - 1) * 2)}- [${text}](${anchor})\n`;
     });
-    textArea.value = textArea.value.substring(0,selectionStart) + result + textArea.value.substring(selectionStart);
+    return result;
+  }
+
+  const addToc = () => {
+    const textArea = document.getElementsByTagName('textarea')[0];
+    const selectionStart = textArea.selectionStart;
+    const toc = createToc(textArea.value);
+    textArea.value = textArea.value.substring(0, selectionStart) + toc + textArea.value.substring(selectionStart);
   };
 
   GM_registerMenuCommand('Add toc', addToc);
