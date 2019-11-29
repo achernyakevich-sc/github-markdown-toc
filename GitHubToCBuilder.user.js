5d7c491f70597a38bef0ba6a67bc77b4d18dffbb
vkuleshov-sc <kuleshov@scand.com>
2019-11-29 13:31:58 +0300

    add tests

--- GitHubToCBuilder.user.js
+++ GitHubToCBuilder.user.js
@@ -13,9 +13,9 @@
 (function () {
   'use strict';
 
-  const getAnchor = text => '#' + text.replace(/ /g, '-').replace(/\/|\.|\(|\)/g, '').toLowerCase();
+  const getAnchor = text => '#' + text.replace(/ /g, '-').replace(/\t/, '--').replace(/[^\d\w-_#]/g, '').toLowerCase();
 
-  const getSharpCount = value => value.match(/#+/)[0].length;
+  const getSharpCount = value => value.match(/#+/) ? value.match(/#+/)[0].length : 0;
 
   const getHeaderText = headerLine => headerLine.replace(/#+\s+/, '');
 
@@ -31,6 +31,56 @@
     });
     return result;
   }
+
+  //{can be deleted
+  (() => {
+    const test = ({ input, output, testingFunc }) => {
+      if (JSON.stringify(testingFunc(input)) !== JSON.stringify(output)) {
+        GM_log(`${testingFunc.name}(${JSON.stringify(input)}) !== ${JSON.stringify(output)}`);
+        GM_log(`${testingFunc.name}(${JSON.stringify(input)}) ==  ${JSON.stringify(testingFunc(input))}`);
+      }
+    };
+    const testCases = [
+      {
+        input: '   123',
+        output: 0,
+        testingFunc: getSharpCount,
+      },
+      {
+        input: '#########################               123',
+        output: 25,
+        testingFunc: getSharpCount,
+      },
+      {
+        input: 'ab(c)?:;\'0!@$-%/\\1_^|23\tDEF',
+        output: '#abc0-1_23--def',
+        testingFunc: getAnchor,
+      },
+      {
+        input: '### header1',
+        output: 'header1',
+        testingFunc: getHeaderText,
+      },
+      {
+        input: '# header1 and some text',
+        output: 'header1 and some text',
+        testingFunc: getHeaderText,
+      },
+      {
+        input: `# header1\r\n### header2 some text\n## header3\r\n`,
+        output: ['# header1', '### header2 some text', '## header3'],
+        testingFunc: getHeadersLines,
+      },
+      {
+        input: `# header1\r\n### header2 some text\n## header3\r\n`,
+        output: '- [header1](#header1)\n    - [header2 some text](#header2-some-text)\n  - [header3](#header3)\n',
+        testingFunc: createToc,
+      },
+    ];
+    testCases.forEach(test);
+
+  })();
+  //can be deleted}
 
   const addToc = () => {
     const textArea = document.getElementsByTagName('textarea')[0];
