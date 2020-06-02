3451f2260c6babbb41ddb3e0ac18d0af03c9b06c
Ilya Navitsky <navitsky@scand.com>
2020-06-02 21:13:24 +0300

    Refactored code to use positive thinking approach.

--- GitHubToCBuilder.user.js
+++ GitHubToCBuilder.user.js
@@ -38,6 +38,7 @@
     let toc = '';
     let anchors = [];
     let hasErrors = false;
+    let errorMsg = '';
     const headerLines = getHeaderLines(mdText);
     if (headerLines) {
       for (let i = 0; i < headerLines.length; i++) {
@@ -47,16 +48,18 @@
         const hAnchor = getHeaderAnchor(hText);
 
         // Check for duplication of header
-        if (-1 != anchors.indexOf(hAnchor)) {
-          return { hasErrors: true, msg: "Headers duplications detected at least for: " + hText };
+        if (-1 == anchors.indexOf(hAnchor)) {
+          anchors.push(hAnchor);
+          toc += `${' '.repeat((hDepth - 1) * 2)}- [${hText}](${hAnchor})\n`;
+        } else {
+          hasErrors = true;
+          errorMsg = "Headers duplications detected at least for: " + hText;
+          break;
         }
-
-        anchors.push(hAnchor);
-        toc += `${' '.repeat((hDepth - 1) * 2)}- [${hText}](${hAnchor})\n`;
       }
     }
 
-    return { hasErrors: false, toc: toc };
+    return { hasErrors: hasErrors, toc: toc, msg: errorMsg };
   }
 
   const getWikiTextAreaElement = () => {
@@ -65,36 +68,32 @@
 
   const copyToCForMarkdownMarkupTextToClipboard = () => {
     const textArea = getWikiTextAreaElement();
-    if (!textArea) {
+    if (textArea) {
+      const result = getToCForMarkdownMarkupText(textArea.value);
+      if (result.hasErrors) {
+        alert(result.msg);
+      } else {
+        GM_setClipboard(result.toc);
+        alert('ToC built from GitHub Wiki page content and copied to the clipboard!');
+      }
+    } else {
       alert('Textarea with Markdown Markup is not detected!');
-      return;
     }
-
-    const result = getToCForMarkdownMarkupText(textArea.value);
-    if (result.hasErrors) {
-      alert(result.msg);
-      return;
-    }
-
-    GM_setClipboard(result.toc);
-    alert('ToC built from GitHub Wiki page content and copied to the clipboard!');
   };
 
   const copyToCForSelectedMarkdownMarkupTextToClipboard = () => {
     const selectedText = document.getSelection().toString();
-    if (selectedText === '') {
+    if (selectedText !== '') {
+      const result = getToCForMarkdownMarkupText(selectedText)
+      if (result.hasErrors) {
+        alert(result.msg);
+      } else {
+        GM_setClipboard(result.toc);
+        alert('ToC built from selected Markdown Markup and copied to the clipboard!');
+      }
+    } else {
       alert('Nothing is selected!');
-      return;
     }
-
-    const result = getToCForMarkdownMarkupText(selectedText);
-    if (result.hasErrors) {
-      alert(result.msg);
-      return;
-    }
-
-    GM_setClipboard(result.toc);
-    alert('ToC built from selected Markdown Markup and copied to the clipboard!');
   };
 
   if (getWikiTextAreaElement()) {
